5fd2e15470331433d914165d1940029bb425cf4b
Shikhar <shikharrajpoot1998@gmail.com>
2021-07-02 15:20:47 +0530

    Updated index.js

--- res/js/index.js
+++ res/js/index.js
@@ -9,21 +9,21 @@ fetch(urlCF)
     for (let x in data) {
       let str = `${data[x].name}`;
       if (str.includes("Codeforces Round"))
-        content.innerHTML += document.write(`${data[x].name} <br>`);
+        content.innerHTML += `${data[x].name} <br>`
     }
   })
   .catch((error) => document.write("Unable to fetch Codeforces Rounds!"));
 
 //Codechef List of Contest
-// let urlCC = "https://kontests.net/api/v1/code_chef";
-// fetch(urlCC)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     for (let x in data) {
-//       let str = `${data[x].name}`;
-//       if (str.includes("2021"))
-//         content.innerHTML += document.write(`${data[x].name} <br>`);
-//     }
-//   })
-//   .catch((error) => document.write("Unable to fetch Codechef Contests!"));
+let urlCC = "https://kontests.net/api/v1/code_chef";
+fetch(urlCC)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    for (let x in data) {
+      let str = `${data[x].name}`;
+      if (str.includes("2021"))
+        content.innerHTML += `${data[x].name} <br>`
+    }
+  })
+  .catch((error) => document.write("Unable to fetch Codechef Contests!"));
